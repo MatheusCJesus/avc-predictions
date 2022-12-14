c50fd86c9113f9351730bac56905a15535d1a9f3
Matheus <matheus.cerqueira100@gmail.com>
2022-12-13 22:07:07 -0300

    First commit, adding all files and notebooks.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repositório para análise e predição de Acidente Vascular Cerebral em Cuiabá-MT',
+    author='Matheus Cerqueira',
+    license='MIT',
+)
